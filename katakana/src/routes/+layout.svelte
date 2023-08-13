3582f5af6e71ddf116a58cfa24923acc49d040f4
tsuyuri104 <tsuyuri104@gmail.com>
2023-08-13 20:46:39 +0900

    レイアウトを作成

---
+++ katakana/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { dev } from '$app/environment';
+	import { firebaseConfig } from '$lib/firebase/config';
+	import 'destyle.css/destyle.css';
+	import { getAnalytics } from 'firebase/analytics';
+	import { initializeApp } from 'firebase/app';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// Initialize Firebase
+		if (!dev) {
+			const app = initializeApp(firebaseConfig);
+			getAnalytics(app);
+		}
+	});
+</script>
+
+<slot />
