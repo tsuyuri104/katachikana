fab7d074e7f307a12613323ef28c41b64bd4f2e8
tsuyuri104 <tsuyuri104@gmail.com>
2023-08-14 16:24:47 +0900

    OGPを設定

--- katakana/src/routes/+layout.svelte
+++ katakana/src/routes/+layout.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	import { dev } from '$app/environment';
+	import { page } from '$app/stores';
 	import { firebaseConfig } from '$lib/firebase/config';
 	import 'destyle.css/destyle.css';
 	import { getAnalytics } from 'firebase/analytics';
 	import { initializeApp } from 'firebase/app';
 	import { onMount } from 'svelte';
+	import OGP from './images/ogp.png';
 
 	onMount(() => {
 		// Initialize Firebase
@@ -14,5 +16,33 @@
 		}
 	});
 </script>
+
+<svelte:head>
+	<title>KATA-CHI-KANA</title>
+	<meta
+		property="og:url"
+		content={$page.url}
+	/>
+	<meta
+		property="og:type"
+		content="article"
+	/>
+	<meta
+		property="og:title"
+		content="KATA-CHI-KANA"
+	/>
+	<meta
+		property="og:description"
+		content="You can type KataKana by symbols."
+	/>
+	<meta
+		property="og:site_name"
+		content="KATA-CHI-KANA"
+	/>
+	<meta
+		property="og:image"
+		content={`${OGP}`}
+	/>
+</svelte:head>
 
 <slot />
